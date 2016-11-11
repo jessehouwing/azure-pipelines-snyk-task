a12009934777ccbd3dffb28a0f3d9dc4f61312dd
Jesse Houwing <jesse.houwing@gmail.com>
2016-11-10 16:41:57 -0800

    Basic structure to call snyk if already available

--- vsts-snyk/vsts-snyk.ts
+++ vsts-snyk/vsts-snyk.ts
@@ -1,2 +1,20 @@
 ﻿///<reference path="./typings/main.d.ts" />
-import tl = require("vsts-task-lib/task");
+import * as tl from "vsts-task-lib/task";
+import * as trm from "vsts-task-lib/toolrunner";
+
+async function run() {
+    try {
+        let snyk = tl.which('snyk');
+        let snykRunner = new trm.ToolRunner(snyk);
+
+        // Set the commandline arguments
+        snykRunner.arg(tl.getInput('samplestring', true));
+
+        await snykRunner.exec();
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
