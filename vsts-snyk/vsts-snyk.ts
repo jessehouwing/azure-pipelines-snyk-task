c90b0f3e5e46941ac0a2634b7ea62f26bc0f8044
Jesse Houwing <jesse.houwing@gmail.com>
2016-11-10 17:19:41 -0800

    Manifest setup, icons and very basic implementation

--- vsts-snyk/vsts-snyk.ts
+++ vsts-snyk/vsts-snyk.ts
@@ -4,13 +4,14 @@ import * as trm from "vsts-task-lib/toolrunner";
 
 async function run() {
     try {
-        let snyk = tl.which('snyk');
-        let snykRunner = new trm.ToolRunner(snyk);
+        const snyk = tl.which('snyk');
+        const snykRunner = new trm.ToolRunner(snyk);
 
         // Set the commandline arguments
-        snykRunner.arg(tl.getInput('samplestring', true));
+        snykRunner.arg(tl.getInput("samplestring", true));
 
-        await snykRunner.exec();
+        const snykResult: number = await snykRunner.exec();
+        tl.setResult(snykResult, tl.loc("SnykReturnCode", snykResult));
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
